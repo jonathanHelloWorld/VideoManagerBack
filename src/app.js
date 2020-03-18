304745ec07ee9b2060f02de9ca10e18e20aebad2
Jonathan Miranda <jmirandamoraes123@gmail.com>
2020-03-18 14:16:13 -0300

    Open chrome full screen from command

--- src/app.js
+++ src/app.js
@@ -1,11 +1,21 @@
-const express = require('express');
+
+import express from 'express';
+import OpenChrome from './services/chromeService'
+
 const app = express();
+
+
+app.use('/static', express.static(process.cwd() + '/public'));
 
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 });
+
+OpenChrome();
+
---
+++ src/services/chromeService.js
@@ -0,0 +1,10 @@
+const cp = require('child_process'),
+    url_to_open = 'http://localhost:3000/static/';
+
+
+
+
+export default function OpenChrome(){
+    console.log("entrou here");
+    cp.spawn('c:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', ['-kiosk', url_to_open]);
+}
